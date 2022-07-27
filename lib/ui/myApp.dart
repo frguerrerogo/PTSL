808ca33bda7fcea62d4e67a11ab33cac15bafe07
frguerrerogo <frguerrerogo@gmail.com>
2022-07-27 07:58:10 -0500

    login page is created, code is created for color, paths, text style and application paths.

---
+++ lib/ui/myApp.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:ptsl/ui/page/login_page.dart';
+import 'package:ptsl/ui/routes/routes.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      initialRoute: 'login',
+      routes: getAppRoutes(),
+    );
+  }
+}
---
+++ lib/ui/theme/text_styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:ptsl/ui/theme/colors.dart';
+
+class AppTextStyle {
+  static String? get fontFamily => "Pokemon";
+
+  static TextStyle get defaultFontStyle => const TextStyle(
+        fontFamily: 'Pokemon',
+      );
+
+  // Headline 1
+  static TextStyle get headline1 => const TextStyle(
+        fontFamily: 'Pokemon',
+        color: AppColors.primaryColor,
+        fontWeight: FontWeight.bold,
+        fontSize: 60,
+      );
+
+  // Headline 2
+  static TextStyle get headline2 => const TextStyle(
+        fontFamily: 'Pokemon',
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+        fontSize: 30,
+      );
+  // Headline 3
+  static TextStyle get headline3 => const TextStyle(
+        fontFamily: 'Pokemon',
+        color: AppColors.secondaryColor,
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      );
+  // Bodytext 1
+  static TextStyle get bodytext1 => const TextStyle(
+        fontFamily: 'Pokemon',
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+        fontSize: 30,
+      );
+
+  static TextTheme get textTheme => TextTheme(
+        headline1: headline1,
+        headline2: headline2,
+        headline3: headline3,
+        bodyText1: bodytext1,
+      );
+}
